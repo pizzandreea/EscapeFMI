88cfdfa0607f6b095d579128cc51e91d8cf76695
cimuletz <alex.serafim@s.unibuc.ro>
2022-11-11 13:21:36 +0200

    Added player movement & collision

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
+    private RaycastHit2D hit;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction, right or left
+
+        if (moveDelta.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Make sure nothing is blocking
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Humans", "Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Humans", "Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
+        // Moving
+
+    }
+}
