329c3080a3591854c80e66a74b26b1273a42ebd7
Luca Trușcă <t.luca1152@gmail.com>
2023-01-07 21:28:46 +0200

    Create a PopupText class

---
+++ Assets/Scripts/Game/PopupText.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class PopupText : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
