fc72f8f4fa525498db89eae3e8a803fc8b8aab03
Luca Trușcă <t.luca1152@gmail.com>
2023-01-08 18:39:25 +0200

    Add a Testing script that creates a PopupText at a game object's position

--- Assets/Scripts/Game/Testing.cs
+++ Assets/Scripts/Game/Testing.cs
@@ -4,5 +4,11 @@ namespace Game
 {
     public class Testing : MonoBehaviour
     {
+        [SerializeField] private Transform pfPopupText;
+
+        private void Start()
+        {
+            Instantiate(pfPopupText, transform.position, Quaternion.identity);
+        }
     }
 }
