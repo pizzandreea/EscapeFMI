a2e964f4c702a5802a7340fb1debb18773f52331
Luca Trușcă <t.luca1152@gmail.com>
2023-01-08 19:21:47 +0200

    Make the popup text fade out

--- Assets/Scripts/Game/PopupText.cs
+++ Assets/Scripts/Game/PopupText.cs
@@ -6,8 +6,12 @@ namespace Game
     public class PopupText : MonoBehaviour
     {
         private const float MoveYSpeed = 0.2f;
-        
+        private const float DisappearSpeed = 3.0f;
+        private const float DisappearTime = 1.0f;
+
         private TextMeshPro _textMesh;
+        private Color _textColor;
+        private float _disappearTimer = DisappearTime;
 
         public static PopupText Create(Vector3 position, string text)
         {
@@ -26,11 +30,25 @@ namespace Game
         private void Setup(string text)
         {
             _textMesh.SetText(text);
+            _textColor = _textMesh.color;
         }
 
         private void Update()
         {
             transform.position += new Vector3(0, MoveYSpeed) * Time.deltaTime;
+            _disappearTimer -= Time.deltaTime;
+            if (_disappearTimer <= 0.0f)
+            {
+                // Start disappearing
+                _textColor.a -= DisappearSpeed * Time.deltaTime;
+                _textMesh.color = _textColor;
+
+                if (_textColor.a <= 0.0f)
+                {
+                    // Disappeared
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 }
