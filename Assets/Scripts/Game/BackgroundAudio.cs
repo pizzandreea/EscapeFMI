508bdefb4627b0642542a98c793c0518a39801b3
Zugravu Alexandru Stefan <alexandruzugra@gmail.com>
2023-01-10 00:15:01 +0200

    Added background audio

---
+++ Assets/Scripts/Game/BackgroundAudio.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
