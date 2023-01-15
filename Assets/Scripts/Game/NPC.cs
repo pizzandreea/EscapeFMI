5d75fac0fcf00cd39b23e695ad2e17430921ca03
Bogdan-Strat <bodo.pa2001@yahoo.com>
2023-01-15 18:57:16 +0200

    dialogue design

--- Assets/Scripts/Game/NPC.cs
+++ Assets/Scripts/Game/NPC.cs
@@ -10,12 +10,51 @@ public class NPC : MonoBehaviour
     public Player player;
     public GameObject dialoguePanel;
     public TextMeshProUGUI dialogueText;
- 
+    public string[] dialogue;
+    private int index = 0;
+
+    public float wordSpeed;
+
+    private void Start()
+    {
+        dialogueText.text = "";
+    }
+    
+    public void RemoveText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            RemoveText();
+        }
+    }
     public void HandleEnterProximity(Collider2D collider)
     {
         if (collider.CompareTag("Player"))
         {
             dialoguePanel.SetActive(true);
+            StartCoroutine(Typing());
         }
     }
 
@@ -23,7 +62,7 @@ public class NPC : MonoBehaviour
     {
         if (collider.CompareTag("Player"))
         {
-            dialoguePanel.SetActive(false);
+            RemoveText();
         }
     }
 }
