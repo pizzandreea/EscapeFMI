29f9107a5f04dc7e054baf1b0416a24921d3601e
mirunaavram <avrammiruna2001@yahoo.com>
2023-01-19 19:51:40 +0200

    quit game

--- Assets/Scripts/Game/MainMenu.cs
+++ Assets/Scripts/Game/MainMenu.cs
@@ -11,5 +11,11 @@ namespace Game
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
     }
 }
