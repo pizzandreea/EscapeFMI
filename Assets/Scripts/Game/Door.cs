57a77333dce0f935f6ae832baec5ce8d54695846
davidbejenariu <72975663+davidbejenariu@users.noreply.github.com>
2023-01-20 02:22:33 +0200

    added more door unlocking requirements

--- Assets/Scripts/Game/Door.cs
+++ Assets/Scripts/Game/Door.cs
@@ -37,9 +37,16 @@ namespace Game
                     _inventory.UseItem("Key");
                     gameObject.layer = 7;
 
-                    if (goToNextLevel && GameManager.Instance.inventory.ItemCount("Key") == 1)
+                    if (goToNextLevel && (GameManager.Instance.inventory.ItemCount("Key") == 1 || SceneManager.GetActiveScene().name != "Floor 0"))
                     {
-                        Invoke(nameof(GoToNextLevel), 1.0f);
+                        if (GameManager.Instance.inventory.ItemCount("Scroll") < GetScrollsCount())
+                        {
+                            StartCoroutine(SendNotification("You should take the scroll before moving to the next level", 3));
+                        }
+                        else 
+                        {
+                            Invoke(nameof(GoToNextLevel), 1.0f);
+                        }
                     }
                     else if (goToNextLevel && GameManager.Instance.inventory.ItemCount("Key") < 1)
                     {
@@ -71,7 +78,14 @@ namespace Game
             {
                 if (goToNextLevel && GameManager.Instance.inventory.ItemCount("Key") == 1)
                 {
-                    Invoke(nameof(GoToNextLevel), 1.0f);
+                    if (GameManager.Instance.inventory.ItemCount("Scroll") < GetScrollsCount())
+                    {
+                        StartCoroutine(SendNotification("You should take the scroll before moving to the next level", 3));
+                    }
+                    else 
+                    {
+                        Invoke(nameof(GoToNextLevel), 1.0f);
+                    }
                 }
                 else if (goToNextLevel && GameManager.Instance.inventory.ItemCount("Key") < 1)
                 {
@@ -90,6 +104,22 @@ namespace Game
         private void GoToNextLevel()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        private int GetScrollsCount()
+        {
+            var scrollsCount = 1;
+                    
+            if (SceneManager.GetActiveScene().name == "Floor 1")
+            {
+                scrollsCount = 2;
+            } 
+            else if (SceneManager.GetActiveScene().name == "Floor 2")
+            {
+                scrollsCount = 3;
+            }
+            
+            return scrollsCount;
         }
     }
 }
